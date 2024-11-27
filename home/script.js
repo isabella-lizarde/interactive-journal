b87d33ed9193fbbdc4b8359665332d97b810965b
Isabella <isabellaj.lizarde@gmail.com>
2024-11-27 03:23:43 -0800

    Implemented calendar
    Calendar will have the option to write a journal entry and track the users mood

---
+++ home/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    contentHeight: "auto",
+    events: [
+      {
+        title: "Journaling Prompt 1",
+        start: "2024-11-25",
+        description: "Write about something that made you smile today.",
+      },
+      {
+        title: "Journaling Prompt 2",
+        start: "2024-11-26",
+        description: "Reflect on a goal you achieved this week.",
+      },
+    ],
+  });
+
+  calendar.render();
+});
+
+// calendar.on("eventClick", function (info) {
+//   alert(info.event.title + "\n" + info.event.extendedProps.description);
+// });
