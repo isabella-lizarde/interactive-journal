7dcbd12c9abb897b214184de5ef6cdad439d7923
Isabella <isabellaj.lizarde@gmail.com>
2024-12-07 01:31:25 -0800

    Selected emotion will display on the users current date

--- home/script.js
+++ home/script.js
@@ -3,31 +3,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   const calendarEl = document.getElementById("calendar");
 
+  const handleAddSelectedEmotion = () => {
+    const selectEmotion = document.getElementById("select-emotion");
+    const selectedEmotion = selectEmotion.value;
+    // console.log(selectedEmotion);
+    if (selectedEmotion) {
+      const eventTitle = selectedEmotion;
+      const today = new Date();
+      const localDate = today.toLocaleDateString("en-CA");
+      console.log(localDate);
+
+      calendar.addEvent({
+        title: eventTitle,
+        start: localDate,
+        classNames: ["fc-h-event", "fc-sticky"],
+      });
+    }
+  };
+
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
     contentHeight: "auto",
-    events: [
-      {
-        title: "Journaling Prompt 1",
-        start: "2024-11-25",
-        description: "Write about something that made you smile today.",
-      },
-      {
-        title: "Journaling Prompt 2",
-        start: "2024-11-26",
-        description: "Reflect on a goal you achieved this week.",
-      },
-    ],
   });
 
   calendar.render();
+
+  document.addEventListener("click", handleAddSelectedEmotion);
 });
 
-const handleEmotionSelector = () => {
-  const selectEmotion = document.getAttribute("value");
-  selectEmotion.forEach((emotion) => {
-    emotion.addEventListener("click", () => {
-      console.log(emotion);
-    });
-  });
-};
+// const handleAddSelectedEmotion = () => {
+//   const selectEmotion = document.getElementById("select-emotion");
+//   const selectedEmotion = selectEmotion.value;
+//   console.log(selectedEmotion);
+//   if (selectedEmotion) {
+
+//   }
+// };
